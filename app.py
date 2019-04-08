29c6e9633cb316fc0ed2d1f7e1ce73e71ffae8ab
kellynm <kpmontgo@ncsu.edu>
2019-04-08 18:33:13 -0400

    Combine csv's

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import os
+import glob
+import pandas
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
+os.chdir("data")
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+#combine all files in the list
+combined_csv = pandas.concat([pandas.read_csv(f, sep = ",", engine = "python") for f in all_filenames ])
+#export to csv
+combined_csv.to_csv( "se_fires_combined.csv", index=False, encoding='utf-8-sig')
